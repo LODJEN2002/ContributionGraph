257b111f409fa644736b70c1c36cfa13e318c5de
Ivan <decel1337@gmail.com>
2023-09-21 15:21:46 +0500

    merge objects

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
   const matrix = [];
   const numRows = 51;
   const numColumns = 7;
-  const objectTime = {}
   const [timeObj, setTimeObj] = useState({})
   const [allObj, setAllObj] = useState({})
-  const [dateArr, setDateArr ] = useState([])
+  const [dateArr, setDateArr] = useState([])
 
   for (let i = 0; i < numColumns; i++) {
     matrix[i] = [];
@@ -20,45 +19,47 @@ function App() {
     }
   }
 
-  let now = new Date()
-  let daysAYear = 450
+  function createObjectTime() {
+    const now = new Date()
+    const daysAYear = 355
+    const objectTime = {}
 
-  for (let i = 0; i <= daysAYear; i++) {
-    let date = new Date(now.getTime() - i * 24 * 60 * 60 * 1000)
+    for (let i = 0; i <= daysAYear; i++) {
+      const date = new Date(now.getTime() - i * 24 * 60 * 60 * 1000)
+      const year = date.getFullYear()
+      const month = String(date.getMonth()).padStart(2, '0')
+      const day = String(date.getDate()).padStart(2, '0')
+      const constructorDate = year + '-' + month + '-' + day
+      objectTime[constructorDate] = 0
+    }
 
-    const year = date.getFullYear()
-    const month = String(date.getMonth()).padStart(2, '0')
-    // const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0')
-
-    let constructorDate = year + '-' + month + '-' + day
-    objectTime[constructorDate] = 0
+    return objectTime
   }
 
   // console.log(objectTime) // 2022-08-15 first date
 
   useEffect(() => {
+    const objectTime = createObjectTime()
+
     fetch("https://dpg.gg/test/calendar.json", {
       method: 'GET',
       // headers: { 'Content-Type': 'application/json' },
     })
       .then(res => res.json())
-      .then(res => setTimeObj(res))
-      .then(setAllObj({ ...objectTime, ...timeObj }))
-      .then(console.log(allObj))
-      .then(setDateArr(Object.keys(allObj)))
-    // .then(res => console.log(res))
+      .then(res => {
+        setTimeObj(res)
+        const updateAllObj = {...objectTime, ...res}
+        setAllObj(updateAllObj)
+        setDateArr(Object.keys(allObj))
+      })
+      .catch(err => console.log(err))
   }, [])
 
-  console.log(allObj)
-
   function buttonClick() {
-    const unitedObject = { ...objectTime, ...timeObj }
-    // console.log(unitedObject)
+    // const unitedObject = { ...objectTime, ...timeObj }
     console.log(allObj)
-    // console.log(Object.keys(unitedObject))
-    // console.log(Object.keys(timeObj))
-    // console.log(testObj2)
+    console.log(dateArr)
+
   }
 
   return (
